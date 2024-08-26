73f40ee4ce6b8025059bd993d2183523682c5752
Christian Sanchez <c.sanchez@scheideanstalt.de>
2024-08-26 13:49:27 +0200

    connection pool

--- services/product.service.js
+++ services/product.service.js
@@ -1,11 +1,14 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
+const pool = require('../libs/postgres.pool');
 class ProductService {
 
   constructor() {
     this.products = [];
     this.generate();
+    this.pool = pool;
+    this.pool.on('error', (err) => console.error(err));
   }
 
   generate() {
@@ -33,8 +36,10 @@ class ProductService {
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    const query = 'SELECT * FROM tasks';
+    const rta = await this.pool.query(query);
+    return rta.rows;
   }
 
   findOne(id) {
